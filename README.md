d39b316d0fa03bfe2b8952b604d357b4830deefc
Juan J. Garcia <juanjo.garcia@gmail.com>
2019-07-11 18:17:29 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# spring-bootstrap-template
+
+This is just a spring starter using bootstrap.
+
+## Running
+
+```
+mvn clan package
+java -jar target/spring-boostrap-template-1.0.0-SNAPSHOT.jar
+```
+
+* http://localhost:8080
+* http://localhost:8080/acutator
+
+## References
+
+1. Configure IntelliJ 
+    1. https://blog.jetbrains.com/idea/2018/04/spring-and-spring-boot-in-intellij-idea-2018-1/
+    1. https://www.jetbrains.com/help/idea/spring-boot.html
---
+++ src/main/java/org/j2gl/springbootstrap/web/MainController.java
@@ -0,0 +1,24 @@
+package org.j2gl.springbootstrap.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    private static final Logger log = LoggerFactory.getLogger(MainController.class);
+
+    @Value("${info.app.description}")
+    private String appName;
+
+    @GetMapping("/")
+    public String homePage(final Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+
+}
