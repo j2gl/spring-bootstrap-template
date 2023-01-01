d63c24a5d47ce2cdd305904b839de31b32959afb
Juan Jose Garcia Lau <juanjo.garcia@gmail.com>
2023-01-01 23:17:11 +0100

    springboot-3: Upgrade to spring-boot 3 and fix web security

--- src/main/java/org/j2gl/springbootstrap/config/SecurityConfig.java
+++ src/main/java/org/j2gl/springbootstrap/config/SecurityConfig.java
@@ -1,36 +1,53 @@
 package org.j2gl.springbootstrap.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true,
-                            securedEnabled = true,
-                            jsr250Enabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("guest").password("{noop}asdf1234").roles("USER");
-        auth.inMemoryAuthentication().withUser("admin").password("{noop}asdf1234").roles("ADMIN");
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 =
+                User.withDefaultPasswordEncoder()
+                        .username("guest")
+                        .password("asdf1234")
+                        .roles("USER")
+                        .build();
+
+        UserDetails user2 =
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("asdf1234")
+                        .roles("ADMIN")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/css/**").permitAll()
-            .antMatchers("/").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .formLogin()
-            // .loginPage("/custom-login")
-            .and()
-            .logout().logoutSuccessUrl("/");
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/", "/css/**", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        //.loginPage("/custom-login")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                        .permitAll());
+        return http.build();
     }
 
 }
